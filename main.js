4565b0f8e7ec62cdd67fccb63fb1545876e6c464
irina777-create <71390012+irina777-create@users.noreply.github.com>
2023-11-03 22:14:45 +0200

    cоздание main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+//Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
+let arr=[2,d,56,dag,78,h,45,tyr,90,2];
+function arraySum(array) {
+    const sum = 0;
+    for (const i = 0; i < array.length; i++) {
+        if (Object.prototype.toString.call(array[i]) === '[object Number]') {
+          sum +=[array[i]];
+        }
+    }
+    return sum/arr.length
+}
+console.log(arraySum/(arr.length));
+//Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).
+// Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
+function doMath(x, znak, y) {
+    const x=4;
+    const y=8;
+
+    if (znak === *) {
+        result = x * y;
+    }
+}
+    const functionResult = doMath();
+}
+
+//Написати функцію заповнення даними користувача двомірного масиву.
+// Довжину основного масиву і внутрішніх масивів задає користувач.
+// Значення всіх елементів всіх масивів задає користувач.
+
+const arr1=[3,56,76,58];
+const arr2=[54,73,88,67];
+const arr3=[];
+function outside (arr1) {
+    function inside (arr2) {
+        return arr1+arr2;
+    }
+    result((arr1)+(arr2)=(arr3));
+}
+
+console.log(arr3);
+
